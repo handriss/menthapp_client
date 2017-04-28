6815d5a952d1c6da1ea7b305db8551bda1669a28
handriss <andrashinkel@gmail.com>
2017-04-28 21:21:08 +0200

    ConnectionChecker class skeleton created

--- app/src/main/java/com/smarthome/menthacontrols/menthapp_client_new/request/ConnectionChecker.java
+++ app/src/main/java/com/smarthome/menthacontrols/menthapp_client_new/request/ConnectionChecker.java
@@ -1,8 +1,27 @@
 package com.smarthome.menthacontrols.menthapp_client_new.request;
 
 
+import com.smarthome.menthacontrols.menthapp_client_new.model.ConnectionStatus;
+
 public class ConnectionChecker {
 
+    private ConnectionStatus connectionStatus;
 
+    public ConnectionChecker() {
+        this.connectionStatus = ConnectionStatus.NO_CONNECTION;
+    }
 
+    public void refreshConnectionStatus(){}
+
+    private Boolean getServiceStatus(){
+        return false;
+    }
+
+    private Boolean getInternetStatus(){
+        return false;
+    }
+
+    private Boolean getNetworkStatus(){
+        return false;
+    }
 }
